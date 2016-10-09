fecb20b7784393cef9c354938cc96012823aba3f
Alejandro Quadri <alejandroquadri@gmail.com>
2016-10-09 18:04:05 -0300

    prueba if secret

--- config/index.js
+++ config/index.js
@@ -1,4 +1,14 @@
+var secreto;
+
+if (process.env.NODE_ENV === 'production'){
+  secreto = 'secret';
+}
+
 module.exports = {
-  //secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret'
-  secret: 'secret'
+  secret: secreto
 };
+
+// module.exports = {
+//   //secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret'
+//   secret: 'secret'
+// };
